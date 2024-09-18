1d2b30946f8059aba38058f9289dee8d3f766b4e
librekcesar <140665209+librekcesar@users.noreply.github.com>
2024-09-18 17:11:11 -0600

    Add files via upload

---
+++ centroide_alto_vol.js
@@ -0,0 +1,8 @@
+var centalto = {
+"type": "FeatureCollection",
+"name": "centroide_alto_vol",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "country": "MEX", "locality": "Cuauhtémoc", "state": "Ciudad de México", "zonificacion": "CHT_9", "catstationid": "1", "station_code": "MX1", "zipcode": "06720", "region": "Metro99", "station_name": "Pantaco", "id": "553", "coverage_type": "Local", "cobertura_diferenciada": "No Diferenciado", "cluster_diferenciado": "ZM", "polygon": "1", "neighborhood": null, "fid": "1110", "area": 4254934.098, "km2": 4.255, "Volumen_total": 1120, "densidad": 263.21974148061105, "promedio_diario": 43, "cobertura_prop": null, "Tiempo": null, "volumen_SUM de Total de paquetes con ETA Cliente": "1120", "volumen_promedio": "43" }, "geometry": { "type": "Point", "coordinates": [ -99.137311821009348, 19.393512555997869 ] } }
+]
+}
---
+++ mx2.js
@@ -0,0 +1,8 @@
+var mx2estat = {
+"type": "FeatureCollection",
+"name": "mx2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -99.142262198750956, 19.640306804333537 ] } }
+]
+}
